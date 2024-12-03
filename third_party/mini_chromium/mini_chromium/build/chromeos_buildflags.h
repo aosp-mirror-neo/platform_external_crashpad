1b6536f89bfd8434bf7bb51c8167aa32ab8573c4
Erwin Jansen <jansene@google.com>
2024-12-03 08:10:17 -0800

    Import mini_chromium
    This imports mini_chromium @ c4bcfd045d8dab7f78c7e17a74672e3509a1f182
    
    Bug: 379373650
    Change-Id: Ibe6d3e99565febf3388be4bd448c60b1bbfde181

---
+++ third_party/mini_chromium/mini_chromium/build/chromeos_buildflags.h
@@ -0,0 +1,22 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Unused stub file to make sure libchrome can compile.
+#ifndef BUILD_CHROMEOS_BUILDFLAGS_H_
+#define BUILD_CHROMEOS_BUILDFLAGS_H_
+
+#include "build/buildflag.h"
+#define MINI_CHROMIUM_INTERNAL_BUILDFLAG_VALUE_IS_CHROMEOS_ASH() (0)
+#define MINI_CHROMIUM_INTERNAL_BUILDFLAG_VALUE_IS_CHROMEOS_LACROS() (0)
+#endif  // BUILD_CHROMEOS_BUILDFLAGS_H_
